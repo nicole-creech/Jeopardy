114251a8186fb2874d0765e69d02d682a747448e
Nicole Creech <creech.ns@gmail.com>
2026-08-30 19:16:33 -0400

    Disable HTTP caching so browsers always fetch the latest client code

--- server.js
+++ server.js
@@ -97,7 +97,10 @@ const server = http.createServer((req, res) => {
   fs.readFile(filePath, (err, data) => {
     if (err) { res.writeHead(404); return res.end('Not found'); }
     const ext = path.extname(filePath);
-    res.writeHead(200, { 'Content-Type': MIME[ext] || 'application/octet-stream' });
+    res.writeHead(200, {
+      'Content-Type': MIME[ext] || 'application/octet-stream',
+      'Cache-Control': 'no-store'
+    });
     res.end(data);
   });
 });
